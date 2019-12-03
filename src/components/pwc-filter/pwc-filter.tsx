7c2a342d5d81329b3f2abd2ec6835969a92b54d3
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2019-12-03 22:04:51 +0300

    name fixes

---
+++ src/components/pwc-filter/pwc-filter.tsx
@@ -0,0 +1,12 @@
+import { Component, Prop, h } from "@stencil/core";
+
+@Component({
+  tag: "pwc-filter",
+  styleUrl: "pwc-filter.css",
+  shadow: true
+})
+export class PwcFilterComponent {
+  render() {
+    return "Placeholder";
+  }
+}
