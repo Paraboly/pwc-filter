7faa541e3f8adb373ef5a8dd3b7f27aba5f08af8
starikcetin <cetinsamedtarik@gmail.com>
2019-12-10 02:30:34 +0300

    integrated dynamic form 0.2.0

--- src/components/pwc-filter/PwcFilterInterfaces.tsx
+++ src/components/pwc-filter/PwcFilterInterfaces.tsx
@@ -1,5 +1,5 @@
 import "@paraboly/pwc-dynamic-form";
-import { FormChangedEventPayload } from "@paraboly/pwc-dynamic-form/dist/types/components/pwc-dynamic-form/DynamicFormEvents";
+import { FormChangedEventPayload } from "@paraboly/pwc-dynamic-form/dist/types/components/pwc-dynamic-form/FormEvents";
 
 export declare module PwcFilterInterfaces {
   export interface FilterChangedEventPayload {
